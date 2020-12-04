10bdd320841d61339cb5b6f4db09e29866c7c746
prasadbhanu703 <prasadbhanu703@gmail.com>
2020-12-04 18:58:52 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+let relatives = ["Bhanu", "Prasad", "Doddipati"];
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList"> List 
+            { relatives.map( (ele,index) => <li key={ele+index}>{ele}</li>) }
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
